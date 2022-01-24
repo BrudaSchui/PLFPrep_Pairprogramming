75996e12e2b2c4dffea0704bd24b15d7b503efde
littlegamer <jneissl27@gmail.com>
2022-01-24 18:52:14 +0100

    Add Database

---
+++ PLFPrep/ChinookDbLib/NorthwindContext.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ChinookDbLib
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+        }
+
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Album> Albums { get; set; } = null!;
+        public virtual DbSet<Artist> Artists { get; set; } = null!;
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Genre> Genres { get; set; } = null!;
+        public virtual DbSet<Invoice> Invoices { get; set; } = null!;
+        public virtual DbSet<InvoiceLine> InvoiceLines { get; set; } = null!;
+        public virtual DbSet<MediaType> MediaTypes { get; set; } = null!;
+        public virtual DbSet<Playlist> Playlists { get; set; } = null!;
+        public virtual DbSet<Track> Tracks { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlite("data source=C:\\Users\\Janik\\Schule\\4.Klasse\\POS\\PLFPrep_Pairprogramming\\PLFPrep\\ChinookDbLib\\chinook_date_as_datetime.sqlite");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Album>(entity =>
+            {
+                entity.ToTable("Album");
+
+                entity.Property(e => e.AlbumId).ValueGeneratedNever();
+
+                entity.HasOne(d => d.Artist)
+                    .WithMany(p => p.Albums)
+                    .HasForeignKey(d => d.ArtistId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            modelBuilder.Entity<Artist>(entity =>
+            {
+                entity.ToTable("Artist");
+
+                entity.Property(e => e.ArtistId).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.CustomerId).ValueGeneratedNever();
+
+                entity.HasOne(d => d.SupportRep)
+                    .WithMany(p => p.Customers)
+                    .HasForeignKey(d => d.SupportRepId);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmployeeId).ValueGeneratedNever();
+
+                entity.Property(e => e.BirthDate).HasColumnType("DATETIME");
+
+                entity.Property(e => e.HireDate).HasColumnType("DATETIME");
+
+                entity.HasOne(d => d.ReportsToNavigation)
+                    .WithMany(p => p.InverseReportsToNavigation)
+                    .HasForeignKey(d => d.ReportsTo);
+            });
+
+            modelBuilder.Entity<Genre>(entity =>
+            {
+                entity.ToTable("Genre");
+
+                entity.Property(e => e.GenreId).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Invoice>(entity =>
+            {
+                entity.ToTable("Invoice");
+
+                entity.Property(e => e.InvoiceId).ValueGeneratedNever();
+
+                entity.Property(e => e.InvoiceDate).HasColumnType("DATETIME");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Invoices)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            modelBuilder.Entity<InvoiceLine>(entity =>
+            {
+                entity.ToTable("InvoiceLine");
+
+                entity.Property(e => e.InvoiceLineId).ValueGeneratedNever();
+
+                entity.HasOne(d => d.Invoice)
+                    .WithMany(p => p.InvoiceLines)
+                    .HasForeignKey(d => d.InvoiceId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Track)
+                    .WithMany(p => p.InvoiceLines)
+                    .HasForeignKey(d => d.TrackId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            modelBuilder.Entity<MediaType>(entity =>
+            {
+                entity.ToTable("MediaType");
+
+                entity.Property(e => e.MediaTypeId).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Playlist>(entity =>
+            {
+                entity.ToTable("Playlist");
+
+                entity.Property(e => e.PlaylistId).ValueGeneratedNever();
+
+                entity.HasMany(d => d.Tracks)
+                    .WithMany(p => p.Playlists)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "PlaylistTrack",
+                        l => l.HasOne<Track>().WithMany().HasForeignKey("TrackId").OnDelete(DeleteBehavior.ClientSetNull),
+                        r => r.HasOne<Playlist>().WithMany().HasForeignKey("PlaylistId").OnDelete(DeleteBehavior.ClientSetNull),
+                        j =>
+                        {
+                            j.HasKey("PlaylistId", "TrackId");
+
+                            j.ToTable("PlaylistTrack");
+                        });
+            });
+
+            modelBuilder.Entity<Track>(entity =>
+            {
+                entity.ToTable("Track");
+
+                entity.Property(e => e.TrackId).ValueGeneratedNever();
+
+                entity.HasOne(d => d.Album)
+                    .WithMany(p => p.Tracks)
+                    .HasForeignKey(d => d.AlbumId);
+
+                entity.HasOne(d => d.Genre)
+                    .WithMany(p => p.Tracks)
+                    .HasForeignKey(d => d.GenreId);
+
+                entity.HasOne(d => d.MediaType)
+                    .WithMany(p => p.Tracks)
+                    .HasForeignKey(d => d.MediaTypeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
